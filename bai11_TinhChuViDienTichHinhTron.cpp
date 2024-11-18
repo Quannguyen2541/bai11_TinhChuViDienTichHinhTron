bdbcbb1449d550a388bea2778209a7e3f49510c6
nquan.5114@gmail.com <nquan.5114@gmail.com>
2024-11-18 09:00:07 +0700

    su dung hang so M_PI de thay PI=3.14

--- bai11_TinhChuViDienTichHinhTron.cpp
+++ bai11_TinhChuViDienTichHinhTron.cpp
@@ -1,19 +1,16 @@
 // bai11_TinhChuViDienTichHinhTron.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#define _USE_MATH_DEFINES
 #include <iostream>
-#include <cmath>
 using namespace std;
 int main() {
 	cout << " Chuong trinh tinh chu vi dien tich hinh tron\n";;
-	double bankinh, PI, chuvi, dientich;
+	double bankinh, chuvi, dientich;
 	cout << "Nhap a (bankinh):\n";
 	cin >> bankinh;
-	cout << "PI= 3.14\n"; 
-	PI = 3.14;
-	
-	chuvi = 2 * PI * bankinh;
-	dientich = (PI * (bankinh * bankinh));
+	// su dung hang so M_PI de khai bao thay vi PI=3.14
+	chuvi = 2 * M_PI  bankinh;
+	dientich = (M_PI* (bankinh * bankinh));
 
 	cout << "chu vi hinh tron la:\n" << chuvi << endl;
 	cout << "dien tich hinh tron la:\n" << dientich << endl;
